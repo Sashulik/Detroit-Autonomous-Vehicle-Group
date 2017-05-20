8e4d018ecbe8be84d9e05065e3315377859d8098
Kamal Kamalaldin <ask@kamalaldin.com>
2017-05-20 14:24:44 -0400

    fix: updated print statement for prot binding

--- test/stream_server_test.py
+++ test/stream_server_test.py
@@ -16,7 +16,7 @@ class VideoStreamingTest(object):
 
         self.server_socket = socket.socket()
         self.ip_address = self.get_ip_address()
-        print("Binding to IP: {}:{}".format(self.ip_address, 8002))
+        print("Binding to IP: {}:{}".format(self.ip_address, 8000))
         self.server_socket.bind(self.ip_address, 8000)
         self.server_socket.listen(0)
         self.connection, self.client_address = self.server_socket.accept()
