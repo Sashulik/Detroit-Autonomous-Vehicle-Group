881cee132364a91ff3d0f626b1e7979e3b56c444
Kamal Kamalaldin <ask@kamalaldin.com>
2017-06-03 15:13:47 -0400

    converted the RC control script to python 3

--- test/rc_control_test.py
+++ test/rc_control_test.py
@@ -10,6 +10,7 @@ class RCTest(object):
 
     def __init__(self):
         pygame.init()
+        screen = pygame.display.set_mode((400, 300))
         self.ser = serial.Serial('/dev/cu.usbmodem1421', 115200, timeout=1)
         self.send_inst = True
         self.steer()
@@ -21,62 +22,69 @@ class RCTest(object):
         #     print("printing char:", chr(1))
         #     time.sleep(1)
         #     if yeah:
-        #         self.ser.write(chr(1))    
+        #         ser.write(chr(1))    
         #     else:
-        #         self.ser.write(chr(0))   
+        #         ser.write(chr(0))   
         #     yeah = not yeah
         # return
         while self.send_inst:
             for event in pygame.event.get():
+                # print("hi")
                 if event.type == KEYDOWN:
                     key_input = pygame.key.get_pressed()
-
-                    # complex orders
-                    if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
-                        print("Forward Right")
-                        self.ser.write(chr(6))
-
-                    elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
-                        print("Forward Left")
-                        self.ser.write(chr(7))
-
-                    elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
-                        print("Reverse Right")
-                        self.ser.write(chr(8))
-
-                    elif key_input[pygame.K_DOWN] and key_input[pygame.K_LEFT]:
-                        print("Reverse Left")
-                        self.ser.write(chr(9))
-
-                    # simple orders
-                    elif key_input[pygame.K_UP]:
-                        print("Forward")
-                        self.ser.write(chr(1))
-
-                    elif key_input[pygame.K_DOWN]:
-                        print("Reverse")
-                        self.ser.write(chr(2))
-
-                    elif key_input[pygame.K_RIGHT]:
-                        print("Right")
-                        self.ser.write(chr(3))
-
-                    elif key_input[pygame.K_LEFT]:
-                        print("Left")
-                        self.ser.write(chr(4))
-
-                    # exit
-                    elif key_input[pygame.K_x] or key_input[pygame.K_q]:
-                        print 'Exit'
-                        self.send_inst = False
-                        self.ser.write(chr(0))
-                        self.ser.close()
-                        break
+                    move_car(key_input, self.ser)
+                    
 
                 elif event.type == pygame.KEYUP:
-                    self.ser.write(chr(0))
+                    key_input = pygame.key.get_pressed()
+                    move_car(key_input, self.ser)
+                    # ser.write(b'0')
 
                 time.sleep(.001)
+
+def move_car(key_input, ser):
+    # complex orders
+    if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
+        print("Forward Right")
+        ser.write(b'6')
+
+    elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
+        print("Forward Left")
+        ser.write(b'7')
+
+    elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
+        print("Reverse Right")
+        ser.write(b'8')
+
+    elif key_input[pygame.K_DOWN] and key_input[pygame.K_LEFT]:
+        print("Reverse Left")
+        ser.write(b'9')
+
+    # simple orders
+    elif key_input[pygame.K_UP]:
+        print("Forward")
+        ser.write(b'1')
+
+    elif key_input[pygame.K_DOWN]:
+        print("Reverse")
+        ser.write(b'2')
+
+    elif key_input[pygame.K_RIGHT]:
+        print("Right")
+        ser.write(b'3')
+
+    elif key_input[pygame.K_LEFT]:
+        print("Left")
+        ser.write(b'4')
+
+    # exit
+    elif key_input[pygame.K_x] or key_input[pygame.K_q]:
+        print('Exit')
+        self.send_inst = False
+        ser.write(b'0')
+        self.ser.close()
+    else:
+        ser.write(b'0')
 
 if __name__ == '__main__':
     RCTest()
