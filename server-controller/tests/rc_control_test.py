c6a4d8a44d6e2df41bd8ee81db07d4526e93b216
Brahm Windeler <brahmwindeler@users.noreply.github.com>
2017-07-08 17:31:30 -0400

    Update rc_control_test.py
    Added pygame_sdl2 import lines to support bug in Mac pygame implementation.

--- server-controller/tests/rc_control_test.py
+++ server-controller/tests/rc_control_test.py
@@ -2,6 +2,14 @@ __author__ = 'Kamal, Mike, Mochan'
 
 import time
 import serial
+
+# For Mac terminal support
+try:
+    import pygame_sdl2
+    pygame_sdl2.import_as_pygame()
+except:
+    pass
+
 import pygame
 from pygame.locals import *
 from ports import get_serial_ports
